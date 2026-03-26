6bd995f60d5409ea0eae81c9e56a38a72eb39cb2
Satwik Gupta <satwik292007@gmail.com>
2026-03-26 23:10:22 +0530

    Add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+OPPOSITE = {
+    'top': 'bottom',
+    'bottom': 'top',
+    'left': 'right',
+    'right': 'left'
+}
+
+# Direction to (row_change, col_change)
+DIRECTIONS = {
+    'top':    (-1, 0),
+    'bottom': (+1, 0),
+    'left':   (0, -1),
+    'right':  (0, +1)
+}
